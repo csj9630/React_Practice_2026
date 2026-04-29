d9565d1954f6f03307d1cfac0bceda03d02a32c8
csj1972 <csj9630@gmail.com~>
2026-04-30 01:58:19 +0900

    1차 세팅완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+//책에서는 클래스형 컴포넌트를 쓰지만 현재는 함수형 컴포넌트를 쓰는 게 맞는다고 하니 고쳐가면서 써보겠다.
+import { useState, useEffect } from "react";
 function App() {
+  //state 대신 useState 사용
+  const [isLoading, setIsLoading] = useState(true);
+
+  //componentDidMount 대신 useEffect 사용
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+      console.log("로딩 완료!");
+    }, 2000);
+
+    // Clean-up: 컴포넌트가 사라질 때 예약된 타이머를 취소합니다.
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []); //useEffect //빈 배열 -> mount 시점에서 한번만 실행하기.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 조건부 렌더링: isLoading이 true면 앞부분, false면 뒷부분 출력 */}
+      {isLoading ? "loading.." : "Ready!"}
+      <p> className Test React at 2026</p>
     </div>
   );
 }
